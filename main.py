0fdca8c669fcf7671c02a116cbf9ef9e629c82a8
Ehab Ahmed (lain-the-coder) <ahmed.ehabj@gmail.com>
2026-03-08 13:34:59 +0400

    Keeping track of input and output tokens by adding usage_metadata property

--- main.py
+++ main.py
@@ -13,7 +13,12 @@ def main():
        model="gemini-2.5-flash",
        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
     )
-    print(response.text)
+    if response.usage_metadata is None:
+       raise RuntimeError("Failed API Request, please try again later")
+    else:
+       print("User prompt: Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+       print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
+       print(f"Response:\n{response.text}")
 
 if __name__ == "__main__":
     main()
